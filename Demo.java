b71dd77ff8978bf5a1b16a4f726bec168980e0df
Anirudh Suresh <113133041+anirudhsuresh21@users.noreply.github.com>
2026-03-02 13:47:45 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Demo{
+	public static void main(String args[]){
+		ArrayList<String> arr = new ArrayList<>();
+		arr.add("Volvo");
+		arr.add("BMW");
+		System.out.println(""+ arr);
+	}
+}
